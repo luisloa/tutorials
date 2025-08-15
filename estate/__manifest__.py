320c3775be01653546e684da5dfec226c6a622c0
Luis Enrique Jimenez Loa <kiqe7913@gmail.com>
2025-08-15 00:49:08 -0600

    Add estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Management',
+    'version': '1.0',
+    'category': 'Tutorials',
+    'summary': 'Module for managing properties (tutorial)',
+    'author': 'Luis Enrique Jimenez Loa',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
