c4fe2d870920bffefcbbad27e6cf42a8cec9cf5e
Luis Enrique Jimenez Loa <kiqe7913@gmail.com>
2025-08-16 17:42:08 -0600

    add in estate_property_views.xml: estate_property_form_view, estate_property_search

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,9 @@
     'summary': 'Module for managing properties (tutorial)',
     'author': 'Luis Enrique Jimenez Loa',
     'depends': ['base'],
+    'data': ['security/ir.model.access.csv',
+             'views/estate_property_views.xml',
+             'views/estate_menus.xml'],
     'installable': True,
     'application': True,
-
-    'data': ['security/ir.model.access.csv',
-             'views/estate_property_views.xml'],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,21 @@
 from odoo import fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Información de propiedade inmobiliaria'
 
-    name = fields.Char(required=True)
+    name = fields.Char(required="True")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, 
+                                    default=date.today() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=False, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,6 +29,19 @@ class EstateProperty(models.Model):
             ('east', 'East'),
             ('west', 'West')
         ])
+    state = fields.Selection(
+        string = 'State',
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='new',
+    )
+    active = fields.Boolean(default=True)
+    
 
 
 
